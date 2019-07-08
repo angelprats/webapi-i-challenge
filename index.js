c44300dd2490f2b44e2e45f02c8bc3fe93c1d7b5
angel prats <angelpratz@gmail.com>
2019-07-08 15:16:38 -0400

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+
+const db = require('./data/db.js');
+
+server.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => console.log(`server is running on port ${port}`));
+
+  
