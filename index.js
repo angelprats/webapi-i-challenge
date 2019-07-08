8a255ad6962a58f00f0b12f4a07641459ae42b46
angel prats <angelpratz@gmail.com>
2019-07-08 16:03:06 -0400

    put and delete requests created no issues so far

--- index.js
+++ index.js
@@ -25,13 +25,47 @@ server.get('/api/users', (req, res) => {
     .catch(err => res.status(500).json({ error: "The user information could not be retrieved." }))
 })
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
     db.findById(id)
     .then(users => res.status(200).json(users))
     .catch(err => res.status(404).json({ message: "The user with specified id does not exist." }));
 })
 
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
 
+    .then(users => {
+        if(users) {
+            res.status(200).end()
+        } else {
+            res.status(404).json({ message: "The user with the specified id does not exist." })
+        }
+    })
+    .catch(err => res.status(500).json({ error: "The user could not be removed!" }))
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db.findById(id)
+    .then(user => {
+        if(user) {
+            if(!changes.name || !changes.bio) {
+                db.update(id, changes)
+                .then(updated => res.status(200).json(updated))
+                .catch(err => res.status(500).json({ error: "The user information could not be modified" }))
+            } 
+            else {
+                res.status(400).json({ error: "Please provide a name and bio for the user." })
+            }
+        }
+            else {
+                res.status(404).json({ message: "The user with the specified id does not exist." })
+            }
+    })
+    .catch(err => res.status(500).json({ error: "The user informatio copuld not be modified" }))
+})
 
 
 const port = process.env.PORT || 8000;
