427ad9db75cdf5b0da13e028ed3e8ab3e9aeaa37
angel prats <angelpratz@gmail.com>
2019-07-11 16:48:07 -0400

    added support for environment variables

---
+++ api/server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+
+const db = require('../data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+server.post('/api/users', (req, res) => {
+    const apiInfo = req.body;
+    if(!apiInfo.name || !apiInfo.bio) {
+        res.status(400).json({message: 'Please provide a name and bio for the user'});
+    } else {
+        db.insert(apiInfo)
+        .then(user => res.status(201).json(user))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }));
+    }
+});
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ error: "The user information could not be retrieved." }))
+});
+
+server.get('/api/users/:id', (req, res) => {
+    db.findById(id)
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(404).json({ message: "The user with specified id does not exist." }));
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.remove(id)
+
+    .then(users => {
+        if(users) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: "The user with the specified id does not exist." });
+        }
+    })
+    .catch(err => res.status(500).json({ error: "The user could not be removed!" }))
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db.findById(id)
+    .then(user => {
+        if(user) {
+            if(!changes.name || !changes.bio) {
+                db.update(id, changes)
+                .then(updated => res.status(200).json(updated))
+                .catch(err => res.status(500).json({ error: "The user information could not be modified" }))
+            } 
+            else {
+                res.status(400).json({ error: "Please provide a name and bio for the user." })
+            }
+        }
+            else {
+                res.status(404).json({ message: "The user with the specified id does not exist." })
+            }
+    })
+    .catch(err => res.status(500).json({ error: "The user information could not be modified" }))
+});
+
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,76 +1,17 @@
 // implement your API here
+const dotenv = require('dotenv');
+dotenv.config();
 
 const express = require('express');
 
-const server = express();
-
-const db = require('./data/db.js');
-
-server.use(express.json());
-
-server.post('/api/users', (req, res) => {
-    const apiInfo = req.body;
-    if(!apiInfo.name || !apiInfo.bio) {
-        res.status(400).json({message: 'Please provide a name and bio for the user'});
-    } else {
-        db.insert(apiInfo)
-        .then(user => res.status(201).json(user))
-        .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }));
-    }
-})
-
-server.get('/api/users', (req, res) => {
-    db.find()
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json({ error: "The user information could not be retrieved." }))
-})
-
-server.get('/api/users/:id', (req, res) => {
-    db.findById(id)
-    .then(users => res.status(200).json(users))
-    .catch(err => res.status(404).json({ message: "The user with specified id does not exist." }));
-})
-
-server.delete('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    db.remove(id)
-
-    .then(users => {
-        if(users) {
-            res.status(200).end()
-        } else {
-            res.status(404).json({ message: "The user with the specified id does not exist." })
-        }
-    })
-    .catch(err => res.status(500).json({ error: "The user could not be removed!" }))
-})
-
-server.put('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    const changes = req.body;
-    db.findById(id)
-    .then(user => {
-        if(user) {
-            if(!changes.name || !changes.bio) {
-                db.update(id, changes)
-                .then(updated => res.status(200).json(updated))
-                .catch(err => res.status(500).json({ error: "The user information could not be modified" }))
-            } 
-            else {
-                res.status(400).json({ error: "Please provide a name and bio for the user." })
-            }
-        }
-            else {
-                res.status(404).json({ message: "The user with the specified id does not exist." })
-            }
-    })
-    .catch(err => res.status(500).json({ error: "The user informatio copuld not be modified" }))
-})
-
-
-const port = process.env.PORT || 8000;
-server.listen(port, () => console.log(`server is running on port ${port}`));
+const server = require('./api/server.js');
 
 
 
-  
+const port = process.env.PORT || 6000;
+server.listen(port, () => {
+    console.log(`\n*** Server is running on http://localhost:${port} ***\n`);
+    });
+
+
+
